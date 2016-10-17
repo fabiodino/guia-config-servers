3219d16e259394fe983cdd092b523fe28bb3a421
Fabio Dino Orchi <dino.orchi@gmail.com>
2016-10-17 19:48:38 -0200

    Update step1.sh

--- step1.sh
+++ step1.sh
@@ -1,1 +1,16 @@
 ## AQUI
+
+Apache2
+sudo apt-get install apache2 libapache2-mod-php7.0
+
+PHP7.0
+sudo apt-get install php7.0 php7.0-mysql
+
+MySQL
+sudo apt-get install mysql-server libapache2-mod-auth-mysql
+
+postgreSQL
+sudo apt-get install postgresql postgresql-contrib
+
+WordPress
+sudo apt-get install postgresql postgresql-contrib
